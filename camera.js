3111ea696e714c8050354f7b5ce17bd455a0401b
Alexis He <alexis.yuhe@gmail.com>
2023-11-17 23:02:44 +0100

    Add tracking.js + boilerplate

--- camera.js
+++ camera.js
@@ -1,5 +1,6 @@
-var LOGS_ID = 'logs';
 // log overriding
+var LOGS_ID = 'logs';
+
 console.log = function (message) {
     var logsDiv = document.getElementById(LOGS_ID);
     var logEntry = document.createElement('div');
@@ -24,43 +25,53 @@ window.onerror = function (message, source, lineno, colno, error) {
 };
 
 // Accessing the back-facing camera
+var VIDEO_ID = 'video';
 navigator.mediaDevices.getUserMedia({
     video: {
-        facingMode: { exact: 'environment' } // or 'user' for front-facing camera
+        facingMode: { exact: 'user' } // or 'user' for front-facing camera
     }
 })
     .then(function (stream) {
-        var video = document.getElementById('video');
+        var video = document.getElementById(VIDEO_ID);
         video.srcObject = stream;
         video.play();
     })
     .catch(function (err) {
-        console.log("Error: " + err);
+        console.log("Error?: " + err);
     });
 
-var video = document.getElementById('video');
+var video = document.getElementById(VIDEO_ID);
 var canvas = document.createElement('canvas');
 var context = canvas.getContext('2d');
-var tracker = new tracking.Tracker('target');
 document.body.appendChild(canvas);
 
-tracking.track('#video', new tracking.ColorTracker(['magenta']));
-
-tracking.ColorTracker.registerColor('magenta', function (r, g, b) {
-    return r > 200 && g < 50 && b > 200;
-});
-
-tracking.track('#video', tracker);
-
-tracker.on('track', function (event) {
-    console.log('huh');
-    
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    event.data.forEach(function (rect) {
-        context.strokeStyle = '#FF0000';
-        context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+window.onload = function() {
+    tracking.ColorTracker.registerColor('black', function(r, g, b) {
+        //if (r < 50 && g > 200 && b < 50) {
+        //    return true;
+        //}
+        return true;
     });
-});
+    //var tracker = new tracking.Tracker('target');
+    var tracker = new tracking.ColorTracker(['black']);
 
-console.log('Done');
+    tracker.on('track', function (event) {
+        if (event.data.length === 0) {
+            // No targets were detected in this frame.
+            //console.log('No detection');
+        } else {
+            console.log('detect');
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            
+            event.data.forEach(function (rect) {
+                //context.strokeStyle = '#FF0000';
+                //context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+               // console.log(rect.x, rect.y, rect.height, rect.width, rect.color);
+            });
+        }
+    });
+
+    tracking.track('#video', tracker, { camera: true } );
+
+    console.log('Done');
+};
